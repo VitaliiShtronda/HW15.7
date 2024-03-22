5fc1c9d8e9abcbd7f656964745b86e5fc9210948
VitaliiShtronda <v.shtronda@ukr.net>
2024-03-22 07:05:08 +0200

    Initial commit

---
+++ src/main/java/org/example/database/Database.java
@@ -0,0 +1,41 @@
+package org.example.database;
+
+import org.example.prop.PropertyReader;
+
+import java.sql.*;
+
+public class Database {
+    private static final Database INSTANCE = new Database();
+    private static final String SQL_EX_MESSAGE = "Cannot create connection. Reason: ";
+
+    private static Connection connection;
+
+    private Database() {
+        String url = PropertyReader.getConnectionURLForPostgres();
+        String user = PropertyReader.getUserForPostgres();
+        String password = PropertyReader.getPasswordForPostgres();
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println(SQL_EX_MESSAGE + e.getMessage());
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public static Database getInstance() {
+        return INSTANCE;
+    }
+
+    public static void closeConnection (){
+        try{
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
